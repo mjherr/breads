e10b7b0704ed2f3aab0105947a2fe170d5152b10
Maria Rios <kanelapr@gmail.com>
2022-07-21 19:00:19 -0400

    sending index and show data

--- views/index.jsx
+++ views/index.jsx
@@ -1,10 +1,24 @@
 const React = require('react')
+const breads = require('../controllers/breads_controller')
 const Default = require('./layouts/Default')
 
 function Index () {
     return (
       <Default>
         <h2>Index Page</h2>
+        {/* <p>I have {breads[0].name} bread!</p> */}
+        <ul>
+            {
+                breads.map((bread, index) => {
+                    return (<li key={index}>
+                        <a href={`/breads/${index}`}>
+                        {bread.name}
+                        </a>
+                    </li>
+                    )
+                })
+            }
+        </ul>
       </Default>
     )
 }
